2872529d21d04e5e9e645a665fb20b47297e8560
thiagohubes <thiago.hubes@gmail.com>
2019-12-31 06:08:40 -0300

    Primeiro commit

---
+++ cmake-build-debug/CMakeFiles/c_programming_language.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_programming_language.dir/main.c.o"
+  "c_programming_language"
+  "c_programming_language.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_programming_language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello.c
@@ -0,0 +1,11 @@
+//
+// Created by Thiago Eugenio Hubes on 31/12/19.
+// Chapter 1 - Program 1
+//
+#include <stdio.h>
+
+int hello() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
